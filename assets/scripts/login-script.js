1d69b907d673e5196e052f5d0cc1cac2e339a3a9
Victor-CN-26 <vicmendzerox4t@gmail.com>
2025-02-13 11:19:56 +0700

    Update login-script.js
    Mengganti komentar sesuai dengan format JSDoc

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -5,21 +5,29 @@
  * Gunakan fungsi showPopUp() untuk menampilkan pop up error
  */
 
+/*input alamat email dan password */
 const loginFormElement = document.querySelector('#loginForm');
 const inputEmailElement = document.querySelector('#inputEmail');
 const inputPasswordElement = document.querySelector('#inputPassword');
 
+/* menerima dan mencocokkan email dan password yang di input*/
 const expectedEmail = 'admin@dicoding.com';
 const expectedPassword = 'superpassword';
 
+/*fungsi tombol login dengan submit*/
 loginFormElement.addEventListener('submit', function(event) {
   event.preventDefault();
 
+  /*memasukkan data email dan password yang sudah di input*/
   const email = inputEmailElement.value;
   const password = inputPasswordElement.value;
+
+  /*kondisi, jika berhasil maka akan di arahkan ke halaman home*/
   if (email == expectedEmail && password == expectedPassword) {
     goToHome();
   } else {
+
+    /*jika tidak berhasil maka akan keluar output inputan salah*/
     showPopUp();
   }
 });
